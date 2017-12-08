416c60b6117d37a22b6bc020fa2098a30f03d3b7
HejinYo <hejinyo@gmail.com>
2017-12-09 01:18:04 +0800

    update

--- src/main/java/cn/hejinyo/learn/netty/server/common/ChatServerInitializer.java
+++ src/main/java/cn/hejinyo/learn/netty/server/common/ChatServerInitializer.java
@@ -14,28 +14,30 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 
-public class ChatServerInitializer extends ChannelInitializer<Channel>{
-	private final ChannelGroup group;
-	
-	public ChatServerInitializer(ChannelGroup group) {
-		this.group = group;
-	}
-	
-	@Override
-	protected void initChannel(Channel ch) throws Exception {
-		ChannelPipeline pipeline = ch.pipeline();
-		//处理日志
-		pipeline.addLast(new LoggingHandler(LogLevel.INFO));
-		
-		//处理心跳
-		pipeline.addLast(new IdleStateHandler(0, 0, 1800, TimeUnit.SECONDS));
-		pipeline.addLast(new ChatHeartbeatHandler());
-		
-		pipeline.addLast(new HttpServerCodec());
-		pipeline.addLast(new ChunkedWriteHandler());
-		pipeline.addLast(new HttpObjectAggregator(64 * 1024));
-		pipeline.addLast(new HttpRequestHandler("/ws"));
-		pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
-		pipeline.addLast(new TextWebSocketFrameHandler(group));		
-	}
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+    private final ChannelGroup group;
+
+    public ChatServerInitializer(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        //处理日志
+        pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+
+        //处理心跳
+        pipeline.addLast(new IdleStateHandler(0, 0, 1800, TimeUnit.SECONDS));
+        pipeline.addLast(new ChatHeartbeatHandler());
+
+        pipeline.addLast(new HttpServerCodec());
+        //支持异步发送大的码流，但不占用过多的内存
+        pipeline.addLast(new ChunkedWriteHandler());
+        //将多个消息转换为单一FullHttpRequest 或者FullHttpResponse,因为Http解码器在每个http消息中会生成多个消息对象
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        pipeline.addLast(new HttpRequestHandler("/ws"));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler(group));
+    }
 }
